3e7d96752a67179bc14d6e2d070c640bc086481f
Thais Martins <thaismartins1999@gmail.com>
2022-05-20 20:02:05 -0300

    fix change color button

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,4 +1,4 @@
-<div class="my-12 h-30 bg-[#003869] rounded py-4 px-2  shadow-2xl  lg:w-[400px] lg:mx-auto m-4">
+<div class="my-12 h-30 bg-[#a81c0b] rounded py-4 px-2  shadow-2xl  lg:w-[400px] lg:mx-auto m-4">
     <a
       class="text-white font-bold text-xl font-poppins text-center"
       href="https://pay.hotmart.com/D65291467L?checkoutMode=0&bid=1644949434713"> 
