c43389b5d7c208e7f6ae71c3f6b9b8e7654a31d3
Thais Martins <thaismartins1999@gmail.com>
2022-05-23 19:58:52 -0300

    resolve conflits

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 <div class="my-12 h-30 bg-[#a81c0b] rounded py-4 px-2  shadow-2xl  lg:w-[400px] lg:mx-auto m-4">
     <a
-=======
-<div class="my-12 h-30 bg-[#003869] rounded py-4 px-2  shadow-2xl  lg:w-[400px] lg:mx-auto m-4">
-
-  <a
->>>>>>> origin/master
       class="text-white font-bold text-xl font-poppins text-center"
       href="https://pay.hotmart.com/D65291467L?checkoutMode=0&bid=1644949434713"> 
       <p>> SIM, EU QUERO COMEÇAR AGORA!</p>
