c297eea54f60c2f0d1429d8eb5f09a47514f9f54
Thais Martins <thaismartins1999@gmail.com>
2022-06-26 12:01:06 -0300

    change link buy

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,7 +1,7 @@
 <div class="my-12 h-30 bg-[#a81c0b] rounded py-4 px-2  shadow-2xl  lg:w-[400px] lg:mx-auto m-4">
     <a
       class="text-white font-bold text-xl font-poppins text-center"
-      href="https://pay.hotmart.com/D65291467L?checkoutMode=0&bid=1644949434713"> 
-      <p>> SIM, EU QUERO COMEÇAR AGORA!</p>
+      href="https://pay.hotmart.com/D65291467L?checkoutMode=10">
+        <p>> SIM, EU QUERO COMEÇAR AGORA!</p>
     </a> 
 </div>
