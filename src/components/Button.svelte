b6114dc316e0db9dbe28949fe9f3396fd7a3a316
Thais Martins <thaismartins1999@gmail.com>
2022-07-14 20:37:45 -0300

    add function addToCart

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,7 +1,16 @@
-<div class="my-12 h-30 bg-[#a81c0b] rounded py-4 px-2  shadow-2xl  lg:w-[400px] lg:mx-auto m-4">
+<script>
+  function addToCart() {
+    fbq('track', 'AddToCart');
+    fbq('track', 'InitiateCheckout');
+    fbq('track', 'CompleteRegistration');
+  }
+</script>
+<div class="my-12 h-30 bg-[#003869] rounded py-4 px-2  shadow-2xl  lg:w-[400px] lg:mx-auto m-4">
+  <button on:click={addToCart}>
     <a
-      class="text-white font-bold text-xl font-poppins text-center"
-      href="https://pay.hotmart.com/D65291467L?checkoutMode=10">
+        class="text-white font-bold text-xl font-poppins text-center"
+        href="https://pay.hotmart.com/D65291467L?checkoutMode=10"> 
         <p>> SIM, EU QUERO COMEÇAR AGORA!</p>
-    </a> 
+      </a> 
+  </button>
 </div>
